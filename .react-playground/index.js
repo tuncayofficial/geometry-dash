62250d28c4e60ee28112d4dde80429a5d5772fd6
Tuncay <68643463+tuncayofficial@users.noreply.github.com>
2022-01-02 20:59:36 +0300

    Delete .react-playground directory

--- .react-playground/index.js
+++
@@ -1,8 +0,0 @@
-
-        import React from 'react';
-        import ReactDom from 'react-dom';
-        import Playground from 'react-playground-kit';
-        import * as module from 'c:/Users/Samsung-2010/Desktop/Tuncay/Projects/portfolio/src/components/Navbar.js';
-
-        ReactDom.render(React.createElement(Playground, { module }), document.getElementById('app'));
-    
